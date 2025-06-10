df82f4147dc92538597e8b48ac5fa9eea962970d
Hassaan Ali <mrhassaan929@gmail.com>
2025-06-11 03:40:29 +0500

    changes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -11,13 +11,20 @@ const Content = ({ items, handleCheck, handleDelete, itemsLength }) => {
           handleDelete={handleDelete}
         />
       ) : (
-        <p className="animate-[frombottom_500ms]"> Your list is Empty</p>
+          <p className="animate-[frombottom_500ms]"> Your list is Empty</p>
+          
       )}
 
-      <p className="text-white text-opacity-35 animate-[frombottom_300ms]">
-        {itemsLength === 0
-          ? "Please add item"
-          : `${itemsLength} List ${itemsLength === 1 ? "item" : "items"}`}
+      <p
+        className={`text-white text-opacity-35 animate-[frombottom_300ms] sticky bottom-2 rounded-full px-4 py-2 ${
+          itemsLength === 0 ? "" : "bg-neutral-800"
+        }`}
+      >
+        {itemsLength === 0 ? (
+          "Please add item"
+        ) : (
+          ` ${itemsLength} ${itemsLength === 1 ? "item" : "items"}`
+        )}
       </p>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center text-center p-4 mt-20 bg-black w-full">
+    <footer className="flex items-center justify-center text-center p-4 mt-20 bg-black w-full">
       <p>Copyright &copy; {new Date().getFullYear()}</p>
-    </div>
+    </footer>
   );
 };
 
